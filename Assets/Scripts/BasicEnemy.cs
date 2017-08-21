ce1c8eb4cab9d477bd2e6a5715c89580576a37a0
andras <andras.szabo.fin@gmail.com>
2017-08-21 08:22:58 +0200

    Super simple damage calculation on hit.

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -2,8 +2,61 @@
 
 public class BasicEnemy : MonoWithCachedTransform
 {
+	public Renderer renderer;
+
+	public Material normalMaterial;
+	public Material hitMaterial;
+
+	public float StartingHP = 10;
+
+	private bool _isHit;
+	private float _elapsedSecondsInHitStun;
+	private float _visualHitStunSeconds = 0.1f;
+
 	private void OnTriggerEnter(Collider other)
 	{
-		//TODO
+		//TODO: there must be a better way to do this,
+		// without GetComponent; via UID...?!
+		GetHit(other.GetComponent<Damage>());
+	}
+
+	private void Update()
+	{
+		if (_isHit)
+		{
+			_elapsedSecondsInHitStun += Time.deltaTime;
+		}
+	}
+
+	private void LateUpdate()
+	{
+		if (_elapsedSecondsInHitStun >= _visualHitStunSeconds)
+		{
+			_isHit = false;
+			SwapMaterials(_isHit);
+		}
+
+		if (StartingHP <= 0)
+		{
+			UnityEngine.Object.Destroy(this.gameObject);
+		}
+	}
+
+	private void GetHit(Damage dmg)
+	{
+		StartingHP -= dmg.damage;
+
+		if (!_isHit)
+		{
+			SwapMaterials(true);
+		}
+	
+		_isHit = true;
+		_elapsedSecondsInHitStun = 0;
+	}
+
+	private void SwapMaterials(bool hit)
+	{
+		renderer.material = hit ? hitMaterial : normalMaterial;
 	}
 }
