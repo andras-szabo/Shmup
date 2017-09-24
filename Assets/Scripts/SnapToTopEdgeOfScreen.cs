80b01d30f38d675efc10274d3f92178c8055bb03
andras <andras.szabo.fin@gmail.com>
2017-09-24 13:06:50 +0200

    Destroying players' bullets just as they leave the screen

---
+++ Assets/Scripts/SnapToTopEdgeOfScreen.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SnapToTopEdgeOfScreen : MonoBehaviour
+{
+	private void Start()
+	{
+		gameObject.transform.position = ViewportUtility.GetWorldPosition(new Vector2(0.5f, 1.1f));
+	}
+}
