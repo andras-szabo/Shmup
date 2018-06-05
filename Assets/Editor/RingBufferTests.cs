1ba293f47eef26fca033593b2fa4ab6c916d5cc2
andras <andras.szabo.fin@gmail.com>
2018-06-05 21:02:11 +0200

    Creating a ring buffer stack, to be used with keeping rewind logs.

---
+++ Assets/Editor/RingBufferTests.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using RingBuffer;
+
+public class RingBufferTests
+{
+	[Test]
+	public void CtorTest()
+	{
+		var buffer = new RingBuffer<int>(50);
+		Assert.IsTrue(buffer != null);
+		Assert.IsTrue(buffer.Capacity == 50);
+		Assert.IsTrue(buffer.Count == 0, buffer.Count.ToString());
+	}
+
+	[Test]
+	public void SimplePushTest()
+	{
+		var buffer = new RingBuffer<int>(50);
+		for (int i = 0; i < 50; ++i)
+		{
+			buffer.Push(i);
+		}
+
+		Assert.IsTrue(buffer.Count == 50, buffer.Count.ToString());
+		for (int i = 0; i < 10; ++i)
+		{
+			var newItem = buffer.Pop();
+			Assert.IsTrue(newItem == 49 - i, string.Format("Exp: {0}, act: {1}", 50-i, newItem));
+		}
+
+		Assert.IsTrue(buffer.Count == 40, buffer.Count.ToString());
+	}
+
+	[Test]
+	public void WrapAroundTest()
+	{
+		var buffer = new RingBuffer<int>(10);
+		for (int i = 0; i < 11; ++i)
+		{
+			buffer.Push(i);
+		}
+		Assert.IsTrue(buffer.Count == 10, buffer.Count.ToString());
+
+		var expected = 10;
+		while (!buffer.IsEmpty)
+		{
+			var newItem = buffer.Pop();
+			Assert.IsTrue(newItem == expected--, newItem.ToString());
+		}
+	}
+
+	[Test]
+	public void WrapAroundTwiceTest()
+	{
+		var buffer = new RingBuffer<int>(10);
+		for (int i = 0; i < 25; ++i)
+		{
+			buffer.Push(i);
+		}
+
+		Assert.IsTrue(buffer.Count == 10, buffer.Count.ToString());
+
+		var expected = 24;
+		while (!buffer.IsEmpty)
+		{
+			var newItem = buffer.Pop();
+			Assert.IsTrue(newItem == expected--, newItem.ToString());
+		}
+
+		Assert.IsTrue(buffer.Count == 0, buffer.Count.ToString());
+
+		for (int i = 0; i < 8; ++i)
+		{
+			buffer.Push(i);
+		}
+
+		expected = 7;
+		while (!buffer.IsEmpty)
+		{
+			var newItem = buffer.Pop();
+			Assert.IsTrue(newItem == expected--, newItem.ToString());
+		}
+	}
+
+	[Test]
+	public void ClearBuffer()
+	{
+		var buffer = new RingBuffer<int>(10);
+		for (int i = 0; i < 10; ++i)
+		{
+			buffer.Push(i);
+		}
+
+		Assert.IsTrue(buffer.Count == 10);
+
+		buffer.Clear();
+
+		Assert.IsTrue(buffer.IsEmpty && buffer.Count == 0);
+	}
+}
---
+++ Assets/RingBuffer/RingBuffer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RingBuffer
+{
+	public class RingBuffer<T>
+	{
+		public int Capacity { get; protected set; }
+		public bool IsEmpty { get; protected set; }
+
+		public int Count
+		{
+			get
+			{
+				if (_endIndex > _startIndex) {	return _endIndex - _startIndex; }
+				if (IsEmpty || _endIndex == 0) { return 0; }
+				return Capacity - (_endIndex - _startIndex);
+			}
+		}
+
+		protected List<T> _list;
+		protected int _startIndex;
+		protected int _endIndex;
+
+		public RingBuffer(int size)
+		{
+			Capacity = size;
+			_list = new List<T>(size);
+			IsEmpty = true;
+		}
+
+		public void Push(T newItem)
+		{
+			if (_list.Count < Capacity)
+			{
+				_list.Add(newItem);
+				_endIndex += 1;
+			}
+			else
+			{
+				var newLastIndexCandidate = FindInsertionIndex();
+				_list[newLastIndexCandidate] = newItem;
+				_endIndex = newLastIndexCandidate + 1;
+
+				if (!IsEmpty && newLastIndexCandidate == _startIndex)
+				{
+					IncrementStartIndex();
+				}
+			}
+
+			IsEmpty = false;
+		}
+			
+		public T Pop()
+		{
+			if (IsEmpty) { throw new System.ArgumentException("Trying to pop from empty ringbuffer."); }
+
+			var lastIndex = _endIndex - 1;
+			var itemToReturn = _list[lastIndex];
+
+			if (lastIndex == _startIndex) { IsEmpty = true; }
+
+			_endIndex -= 1;
+			if (_endIndex == 0) { _endIndex = Capacity; }
+
+			return itemToReturn;
+		}
+
+		public void Clear()
+		{
+			_list.Clear();
+			_startIndex = 0;
+			_endIndex = 0;
+			IsEmpty = true;
+		}
+
+		protected void IncrementStartIndex()
+		{
+			if (++_startIndex == Capacity)
+			{
+				_startIndex = 0;
+			}
+		}
+
+		protected int FindInsertionIndex()
+		{
+			var newLastIndexCandidate = _endIndex;
+			if (newLastIndexCandidate == Capacity)
+			{
+				newLastIndexCandidate = 0;
+			}
+			return newLastIndexCandidate;
+		}
+	}
+}
