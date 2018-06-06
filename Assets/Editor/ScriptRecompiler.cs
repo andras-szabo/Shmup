cd33bea66f022857eb62205736fd7c4a58110290
andras <andras.szabo.fin@gmail.com>
2018-06-06 13:25:57 +0200

    Added script to reimport all scripts, to force recompile which sometimes fails, apparently (seems to be a bug related to VS)

---
+++ Assets/Editor/ScriptRecompiler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+public static class ScriptRecompiler
+{
+	[MenuItem("Tools/Recompile scripts #&r")]
+	public static void Recompile()
+	{
+		AssetDatabase.StartAssetEditing();
+
+		foreach (var script in FindAllScripts())
+		{
+			AssetDatabase.ImportAsset(script, ImportAssetOptions.ForceUpdate);
+		}
+
+		AssetDatabase.StopAssetEditing();
+
+		UnityEngine.Debug.Log("[ScriptRecomplier] Finished recompiling scripts.");
+
+		//TODO: Add sound effect, a la UE4
+	}
+
+	private static IEnumerable<string> FindAllScripts()
+	{
+		return AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".cs"));
+	}
+}
